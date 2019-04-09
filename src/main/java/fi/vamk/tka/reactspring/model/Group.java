02b70033e71cbbf3240e074b4001b873207121fe
Joutsenvuori <e1600749@edu.vamk.fi>
2019-04-09 09:01:20 +0300

    first commit

---
+++ src/main/java/fi/vamk/tka/reactspring/model/Group.java
@@ -0,0 +1,48 @@
+package fi.vamk.tka.reactspring.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "user_group")
+public class Group {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NonNull
+    private String name;
+    private String address;
+    private String city;
+    private String stateOrProvince;
+    private String country;
+    private String postalCode;
+    @ManyToOne(cascade=CascadeType.PERSIST)
+    private User user;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade=CascadeType.ALL)
+    private Set<Event> events;
+
+ 
+
+    /**
+     * @return the events
+     */
+    public Set<Event> getEvents() {
+        return events;
+    }
+
+    /**
+     * @param events the events to set
+     */
+    public void setEvents(Set<Event> events) {
+        this.events = events;
+    }
+}
